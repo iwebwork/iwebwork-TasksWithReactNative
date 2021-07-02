f3bd1f1bf39baf5fdbf524dfc78e424939a293a2
Igor <iwebwork.adm@gmail.com>
2021-07-01 21:04:13 -0300

    Copia dos trabalhos finalizado

---
+++ src/communStyles.js
@@ -0,0 +1,13 @@
+export default {
+    fontFamily:'Lato',
+    colors:{
+        primary:'#FFF',
+        secondary:'#EEEDDD',
+        mainText:'#000',
+        subText:'#444',
+        today:'#B13B44',
+        tomorrow:'',
+        iconArea:'#B22222'
+    },
+    fontSize:20
+}
