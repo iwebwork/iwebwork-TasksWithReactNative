3b649fc43130298295beac0fb454680e21f2f39f
Igor <iwebwork.adm@gmail.com>
2021-07-09 00:16:09 -0300

    Ajuste na exibição das mensagens, para que posso exibir as mensagens retornadas do servidor.

--- front-end/src/common.js
+++ front-end/src/common.js
@@ -3,7 +3,11 @@ import {Alert} from 'react-native'
 const server = 'http://192.169.0.112:3000'
 
 function showError(err){
-    Alert.alert('Ops, ocorreu um problema!', `${err}`)
+    if(err.response && err.response.data){
+        Alert.alert('Ops, ocorreu um problema!', `${err.response.data}`)
+    }else{
+        Alert.alert('Ops! Ocorreu um erro inesperado!', 'Tente novamente, caso persista, contate nosso suporte!')
+    }
 }
 
 function showSuccess(msg){
